a015216234891f6b579b772c43f7364a35145395
Santosh <santosharakere@gmail.com>
2017-07-11 22:14:07 +0100

    demo of main and renderer processes

---
+++ app/main.js
@@ -0,0 +1,29 @@
+const electron = require('electron');
+// console.log(Object.keys(electron));
+let { app, BrowserWindow } = electron;
+
+let mainWindow = null;
+
+app.on('ready', () => {
+  console.log('The application is ready');
+  mainWindow = new BrowserWindow({ 
+    width: 300,
+    height:600,
+    titleBarStyle: 'hidden-inset',
+    show: false
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  // mainWindow.loadURL('https://github.com');
+
+});
+
+console.log('Hello World!');
