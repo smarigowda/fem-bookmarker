2a2ba258130effde9563b7d7bb51113d1d2f9f39
Santosh <santosharakere@gmail.com>
2017-07-15 18:20:50 +0100

    successful in getting title from html page

--- app/renderer.js
+++ app/renderer.js
@@ -6,11 +6,21 @@ newLinkUrl.addEventListener('keyup', () => {
   newLinkSubmit.disabled = !newLinkUrl.validity.valid;
 });
 
+const parser = new DOMParser();
+const parseResponse = text => parser.parseFromString(text, 'text/html');
+const findTitle = nodes => nodes.querySelector('title').textContent;
+
 newLinkForm.addEventListener('submit', () => {
+  console.log('submit event...');
   event.preventDefault();
   const url = newLinkUrl.value;
   console.log(url);
   fetch(url)
     .then(response => response.text())
-    .then(console.log);
+    // .then(console.log)
+    .then(parseResponse)
+    // .then(console.log)
+    .then(findTitle)
+    .then(title => console.log(title))
+    .catch(error => console.log(error));
 });
