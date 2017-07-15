4aff4cd0c8e9fb1a36bdf98a490b113b40dac3c2
Santosh <santosharakere@gmail.com>
2017-07-15 22:06:25 +0100

    adding title and url to the page

--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,8 @@
 const newLinkUrl = document.querySelector('#new-link-url');
 const newLinkSubmit = document.querySelector('.new-link-form--submit');
 const newLinkForm = document.querySelector('.new-link-form');
+const linkTemplate = document.querySelector('#link-template');
+const linksSection = document.querySelector('.links');
 
 newLinkUrl.addEventListener('keyup', () => {
   newLinkSubmit.disabled = !newLinkUrl.validity.valid;
@@ -10,6 +12,17 @@ const parser = new DOMParser();
 const parseResponse = text => parser.parseFromString(text, 'text/html');
 const findTitle = nodes => nodes.querySelector('title').textContent;
 
+const addToPage = ({ title, url }) => {
+  const newLink = linkTemplate.content.cloneNode(true);
+  const titleElement = newLink.querySelector('.link-title');
+  const urlElement = newLink.querySelector('.link-url');
+  titleElement.textContent = title;
+  urlElement.href = url;
+  urlElement.textContent = url;
+  linksSection.appendChild(newLink);
+  return ({ url, element });
+};
+
 newLinkForm.addEventListener('submit', () => {
   console.log('submit event...');
   event.preventDefault();
@@ -17,10 +30,10 @@ newLinkForm.addEventListener('submit', () => {
   console.log(url);
   fetch(url)
     .then(response => response.text())
-    // .then(console.log)
     .then(parseResponse)
-    // .then(console.log)
     .then(findTitle)
+    .then(title => ({ title, url }))
+    .then(addToPage)
     .then(title => console.log(title))
     .catch(error => console.log(error));
 });
