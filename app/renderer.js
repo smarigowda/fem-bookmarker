52f182787f0178d03b2183408df6621c144576bc
Santosh Marigowda <santosharakere@gmail.com>
2018-08-11 17:14:18 +0100

    rendering using html template element

--- app/renderer.js
+++ app/renderer.js
@@ -3,6 +3,11 @@ console.log(urlInput);
 const submitButton = document.querySelector('.new-link-form--submit');
 const form = document.querySelector('.new-link-form');
 
+const linkTemplate = document.querySelector('#link-template');
+console.dir(linkTemplate);
+const linkSection = document.querySelector('.links');
+
+
 urlInput.addEventListener('keyup', () => {
   // console.log('event triggerred');
   submitButton.disabled = !urlInput.validity.valid;
@@ -11,8 +16,19 @@ urlInput.addEventListener('keyup', () => {
 const parser = new DOMParser();
 
 const parseResponse = text => parser.parseFromString(text, 'text/html');
-
 const findTitle = nodes => nodes.querySelector('title').text;
+
+const addToPage = ({ title, url }) => {
+  const newLink = linkTemplate.content.cloneNode(true);
+  const titleElement = newLink.querySelector('.link--title');
+  const urlElement = newLink.querySelector('.link--url');
+
+  titleElement.textContent = title;
+  urlElement.href = url;
+  urlElement.textContent = url;
+  linkSection.appendChild(newLink);
+  return { title, url };
+};
 
 form.addEventListener('submit', event => {
   event.preventDefault();
@@ -22,6 +38,8 @@ form.addEventListener('submit', event => {
     .then(response => response.text())
     .then(parseResponse)
     .then(findTitle)
+    .then(title => ({ title, url }))
+    .then(addToPage)
     .then(console.log)
     .catch(error => console.error(error));
 });
